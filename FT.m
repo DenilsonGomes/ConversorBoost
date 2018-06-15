8be009f6f5d10cc24c125419927a7a1b0d1df5a4
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-14 22:54:30 -0300

    Função de Transferencia corrigida

--- FT.m
+++ FT.m
@@ -1,24 +1,28 @@
-%Autor: Denilson Gomes Vaz da Silva
-%Graduando em Engenharia da Computação
-%Controle de Sistemas Dinamicos - Prof. Dr. Vandilberto
-%Função que obtem a FT apartir dos parametros.
+% Autores: Denilson Gomes Vaz da Silva, Diego Martins Gomes Saraiva
+% Graduandos em Engenharia da Computação e Engenharia Elétrica
+% Controle de Sistemas Dinâmicos - Prof. Dr. Vandilberto
+% Função que obtém um FT apartir dos parametros.
 
-%entre com os parametros
-Vi = 15;
+clear
+clc
+
+% Entre com parametros
+Vi = 15 ; %Tensao de entrada
 Vout = 30;
-Vref = 3;
-L = 937e-6;
-Ro = 10;
-D = 0.5;
-C = 330e-6;
+Vref = 3 ;
+L = 937e-6 ;
+Ro = 10 ;
+D = 0.5 ;
+C = 330e-6 ;
 
-numf = [Vi*L/(Ro*(1-D)^2) Vi];
-denf = [C*L L/Ro (1-D)^2];
+numf = [-(Vi * L / (Ro * ( 1 -D) ^ 2)), Vi];
+denf = [C*L , L / Ro ,( 1- D) ^ 2];
 
-F = tf(numf, denf) %Função transferencia da planta
+F = tf (numf, denf) %função transferencia da planta
 
-H = Vref/Vout; %Sensor
-fm = 1/3.3; % Inverso da amplitude do sinal de PWM
-FTMA = F*H*fm; %Função tranferencia de malha aberta
+H = Vref / Vout; % Sensor
+fm = 1 / 5; % Inversão da amplitude do sinal de PWM
+FTMA = F * H * fm; % De Tranferencia de malha aberta
 
-bode(FTMA)
+bode (FTMA)
+pidtune(FTMA,'pid')
